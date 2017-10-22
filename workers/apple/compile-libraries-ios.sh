ae02e9c9b38c49ca8b3b0809ecb2b5ee605d5b06
Colin Snover <github.com@zetafleet.com>
2017-10-21 23:04:26 -0500

    Add third-party libraries to iOS compiler

---
+++ workers/apple/compile-libraries-ios.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./compile-libraries.sh arm-apple-darwin11 /opt/ioscross $@
--- workers/common/library-rules/mpeg2dec.sh
+++ workers/common/library-rules/mpeg2dec.sh
@@ -1,6 +1,22 @@
+get_dependencies automake libtool
 do_fetch
-# mpeg2dec assumes that a powerpc host will have altivec, but this is not the
-# case for the Nintendo CPU
-sed -i 's/have_altivec=yes/have_altivec=no/' configure
+
+if [ "$host" == "powerpc-eabi" ]; then
+	# mpeg2dec assumes that a powerpc host will have altivec, but this is not the
+	# case for the Nintendo CPU
+	sed -i 's/have_altivec=yes/have_altivec=no/' configure
+fi
+
 do_configure
+
+if [[ $host == arm-apple-darwin* ]]; then
+	# the ARM assembler for iOS cannot process .internal directives, so undo the
+	# patch that adds them or compilation will fail
+	patch -R -p1 < debian/patches/60_arm-private-symbols.patch
+
+	# We only want the library, and the utilities fail to compile for an unclear
+	# reason
+	echo "all install clean:" > src/Makefile
+fi
+
 do_make
