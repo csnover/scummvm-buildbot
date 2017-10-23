2be56fb841718f17c4679fbbbec8fdab2b3917ff
Colin Snover <github.com@zetafleet.com>
2017-10-22 21:50:58 -0500

    Don't build libFLAC++ test or examples when compiling flac
    Since flac would not compile on GC/Wii anyway at this time, get
    rid of the altivec-disabling flag.

--- workers/common/library-rules/flac.sh
+++ workers/common/library-rules/flac.sh
@@ -1,3 +1,8 @@
 do_fetch
-do_configure --disable-altivec
+do_configure --disable-thorough-tests
+# We do not need these tests or these examples and depending on how the compiler
+# was set up (like the Android compiler) they may fail to link properly even
+# though the libraries were built fine
+echo "all install clean:" > src/test_libFLAC++/Makefile
+echo "all install clean:" > examples/Makefile
 do_make
