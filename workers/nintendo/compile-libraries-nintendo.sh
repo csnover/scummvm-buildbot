9d46974112f7d985cf032a02789388d0d3bc46f4
Colin Snover <github.com@zetafleet.com>
2017-11-01 00:16:55 -0500

    Remove DS compiler from Nintendo worker
    Until someone can fix up the port to actually work again, there is
    no reason to hold onto this extra compiler code that we don't need
    at the moment. It is easy enough to copy over and recompile again
    if needed in the future.

--- workers/nintendo/compile-libraries-nintendo.sh
+++ workers/nintendo/compile-libraries-nintendo.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
 ./compile-libraries.sh powerpc-eabi /opt/devkitpro/devkitPPC $@
-
-nds_flags="-march=armv6k -mtune=mpcore -mfloat-abi=hard"
-CPPFLAGS=$nds_flags LDFLAGS=$nds_flags ./compile-libraries.sh arm-none-eabi /opt/devkitpro/devkitARM $@
