4c67310ed3b6d0462f619a4c3c8026a5611bb611
Colin Snover <github.com@zetafleet.com>
2017-11-23 15:43:15 -0600

    Fix Android worker builds for Android 23+
    1. Android 23+ refuses to allow non-PIC code in shared libraries,
       so these builds were failing because of libmpeg2's custom
       assembly, which is now disabled.
    
    2. Now prefer building armv7-a builds instead of the older armv4
       builds, since v7a is faster, avoids buggy legacy ARM assembly
       (see ScummVM Trac#6132, Trac#6810, Trac#6957), and should work
       on nearly all Android devices built since 2011.
    
    3. Put environment flags needed to build Android binaries into
       Dockerfile so it is easier to run custom builds using
       `docker exec`.
    
    4. Persist signing keys to a volume, since whenever these change
       Android will refuse to install an updated package unless the old
       one gets uninstalled first. In the future we will need to be
       using real signing keys for builds anyway, so this is the first
       step toward doing so.

--- workers/android/library-rules/mpeg2dec.sh
+++ workers/android/library-rules/mpeg2dec.sh
@@ -3,5 +3,8 @@ do_fetch
 rm -rf .auto
 dh_update_autotools_config
 autoreconf -i
+# mpeg2dec includes non-PIC ARM assembly which is forbidden in Android 23+, so
+# disable it from being used
+sed -i 's/arm\*)/xxarmxx)/' configure
 do_configure
 do_make
