55d0cc781f15ab8545315a235934c4d178bdd834
Colin Snover <github.com@zetafleet.com>
2017-11-23 23:27:58 -0600

    Remove unwanted dynamic dependencies from Windows worker

--- workers/windows/library-rules/libsdl2.sh
+++ workers/windows/library-rules/libsdl2.sh
@@ -2,7 +2,11 @@ do_fetch
 # Enabling shared libraries for SDL only since it frequently needs to be
 # replaced with less buggy earlier/later versions
 ./configure --prefix=$prefix --host=$host
-do_make
+# We do not want to have to include libgcc DLL at runtime, so we need
+# -static-libgcc, but libtool strips things from $LDFLAGS because SDL2
+# is a library build, so we have to pass it using this special -Wc syntax to
+# keep libtool from stripping it
+make -j$num_cpus LDFLAGS="$LDFLAGS -Wc,-static-libgcc" install
 $STRIP /usr/$host/bin/SDL2.dll
 # If --disable-static is passed, compilation will fail because libSDL2main.a
 # won't be built, so just delete the static library so the linker always uses
