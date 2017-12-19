dfdf7419ad1f85738123fbe258cf034048d35505
Colin Snover <github.com@zetafleet.com>
2017-12-18 22:08:19 -0600

    Add FluidSynth for macOS worker

---
+++ workers/macos/library-rules/fluidsynth-lite.sh
@@ -0,0 +1,10 @@
+get_dependencies ca-certificates cmake wget
+wget -O - --progress=bar:force:noscroll \
+	https://github.com/dotfloat/fluidsynth-lite/archive/cdc8115d4f2d6e8280759bbe514ba15fa88053ea.tar.gz | tar -xzf -
+cd fluidsynth-lite*/
+patch -p1 < ../fluidsynth-lite-macos.patch
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+	-DCMAKE_SYSTEM_NAME=Darwin \
+	-DBUILD_SHARED_LIBS=no .
+do_make
+cp -a src/libfluidsynth.a $prefix/lib
